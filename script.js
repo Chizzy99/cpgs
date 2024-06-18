e7b261efc4294a7a37d1b66f65f3b73f0e066d3f
Chizzy99 <schesworth1@aol.com>
2024-06-18 18:59:33 +0100

    Hall of fame page complete. JS added. ToDO committee page.suggestions? Deployment todo.

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const winners = document.getElementById("wall-of-fame");
  
 
 button.addEventListener('click', () => {
-    scrollTo(0,820).winners({behaviour: "smooth"});
+    scrollTo(0,850).winners({behaviour: "smooth"});
 });
