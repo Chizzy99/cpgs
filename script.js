8cca9b403ad8b5a0fb977fc7a12b3bc6b917f7bd
Chizzy99 <schesworth1@aol.com>
2024-06-23 20:36:37 +0100

    All code updated, past champions file added.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const button = document.querySelector("button");
-const winners = document.getElementById("wall-of-fame");
+const show = document.querySelector(".tour-champion");
+const button = document.querySelector(".tour-btn");
 
- 
 
-button.addEventListener('click', () => {
-    scrollTo(0,850).winners({behaviour: "smooth"});
+button.addEventListener('click', function() {
+    show.style.display = 'block';
+    
+button.addEventListener('click', function() {
+    show.style.display = 'none';
+    button.style.display= 'none';
+    
 });
+});
+
+
+
+
