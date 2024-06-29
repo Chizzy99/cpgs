a6280d17d6032fdad997e84bee66a0e7cc67f4c4
Chizzy99 <schesworth1@aol.com>
2024-06-29 05:58:56 +0100

    Commitee HTML added. code updated.

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-const show = document.querySelector(".champion");
+const show = document.querySelector("#champion");
 const button = document.querySelector(".tour-btn");
-const container = document.querySelector(".fame-container");
+const close = document.querySelector(".btn")
+const clear = document.querySelector(".remove")
 
-
-    
 
 button.addEventListener('click', function() {
     show.style.display = 'block';
-    
-button.addEventListener('click', function() {
+    button.style.display = 'none';
+});    
+
+clear.addEventListener('click', function() {
     show.style.display = 'none';
-    button.style.display = 'none';  
+    
 });
-});
+
+
+// button.addEventListener('click', () => {
+//     clear.style.display = 'none';
+// })
+
+
+// button.addEventListener('click', function() {
+//     show.style.display = 'block';
+    
+// button.addEventListener('click', function() {
+//     show.style.display = 'none';
+//     button.style.display = 'none';  
+// });
+// });
 
